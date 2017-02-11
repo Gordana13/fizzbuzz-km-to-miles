9c3a5d8d7ed46ba3c8bc0e092c49665480c19ff7
Gordana13 <gordana.r13@gmail.com>
2017-02-11 14:56:54 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,14 @@
 # -*- encoding: utf-8 -*-
 stevilo = 0
 
-while stevilo <= 100:
-    stevilo = int(raw_input("Vnesite število med 1 in 100: "))
-    for stevilo in range (1, stevilo + 1):
-        print(stevilo)
+stevilo = int(raw_input("Vnesite število med 1 in 100: "))
+for x in range (1, stevilo + 1):
 
-        if stevilo % 3 == 0 and stevilo % 5 == 0:
-            print("fizzbuzz")
-            continue
-        elif stevilo % 5 == 0:
-            print("buzz")
-        elif stevilo % 3 == 0:
-            print("fizz")
-    break
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 5 == 0:
+        print("buzz")
+    elif x % 3 == 0:
+        print("fizz")
+    else:
+        print(x)
